b5e83f9cb54a6e3c677b70a70f6c47bbd90f5c53
Tory Law <t.law@torypages.com>
2013-08-07 10:20:41 -0400

    fixed file name typo

---
+++ redditImgurDl.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python2.7
+
+# this script simply goes through a reddit subreddit, and downloads all the imgur images
+# that have been posted in the subreddit, including albums, and throws all the images
+# into a single folder.
+
+# the script isn't designed to be easy to use, there are a few things that need to be
+# configured, mostly, the download folder and the user agent
+
+# --usage--
+# subreddit less the "/r"
+# post limit
+
+# reddit API
+import praw
+
+# searching through HTML
+from lxml import etree
+
+# getting stuff from the Internet
+import urllib2
+
+# determining script path
+import os
+
+# determining path of script
+import inspect
+
+# built in delay to be nice to the servers
+import time
+
+# for command line arguments
+import sys
+
+
+def imageDownload(img_url):
+    filename =  img_url.rsplit('/', 1)[1]
+    print filename
+    if not os.path.exists(script_path + dl_folder + filename):
+        time.sleep(10)
+        opener = urllib2.build_opener()
+        response = opener.open(img_url)
+        f = open(script_path + dl_folder + filename, 'w')
+        f.write(response.read())
+        f.close
+        return True
+    return False
+
+
+
+
+subreddit = str(sys.argv[1])
+limit = int(sys.argv[2])
+
+# this isn't meant to be user friendly
+if len(subreddit) < 1 or limit < 1:
+    print "Argument Error: quitting"
+    exit (1)
+
+script_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/"
+# this folder must be manually created
+dl_folder = 'dls/'
+
+# there must be a file (useragent.txt) in the directory containing a user agent on the first line
+user_agent = ""
+try:
+    f = open (script_path + "useragent.txt", 'r')
+    user_agent = f.readline()
+    if user_agent == "":
+        raise
+except:
+    print "User agent error"
+    exit (1)
+
+r = praw.Reddit(user_agent=user_agent)
+submissions = r.get_subreddit(subreddit).get_new(limit=limit)
+new_count = 0
+for x in submissions:
+    url = x.__getattribute__('url')
+
+    if url[-3:] == 'jpg' or url[-3:] == 'png' or url[-3:] == 'gif':
+        # just download, we have a direct link
+        imageDownload(url)
+    else:
+        opener = urllib2.build_opener()
+        try:
+            response = opener.open(url)
+            html = response.read()
+            tree = etree.HTML(html)
+            els = tree.xpath("//div[@class='item view album-view-image-link']/a/@href")
+            if len(els) == 0:
+                els = tree.xpath("//a[@class='zoom']/@href")
+
+            if len(els) == 0:
+                els = tree.xpath("//div[@id='image']/div/a/@href")
+
+            # if the link isn't imgur, this should be fine, it will just be a loop of zero
+            for img_url in els:
+                if imageDownload(img_url):
+                    new_count += 1
+        except:
+            pass
+
+print "%s new photos were added" % (str(new_count))
+
